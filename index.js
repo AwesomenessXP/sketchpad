d312a2f24a42982ff0102cdb51a8ddac1be71a5f
HaskMoney <heartstring85@gmail.com>
2022-06-14 22:19:39 -0700

    added MC and ghost mode

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const grid = document.getElementById('container-1'); // this is the container that will contain the grids
+// this is the container that will contain the grids
+const grid = document.getElementById('container-1'); 
 grid.style.flexDirection = "row";
 
-let gridCellNum = 64; // drawing pad will be gridCellNum * gridCellNum
+let gridCellNum = 32; // drawing pad will be gridCellNum * gridCellNum
 
-for (let j = 0; j < gridCellNum; j++) {
-    const gridColumn = document.createElement('div');
+// ------------------- PREPARE THE GRID ---------------------------------------------------------
+
+// algorithm to make n*n grid of squares
+for (let j = 0; j < gridCellNum; j++) { // creating n number of columns
+    const gridColumn = document.createElement('div'); 
     gridColumn.style.display = "flex";
     gridColumn.style.flex = '1 1 auto'; // height and width will automatically adjust
-    gridColumn.id = `column-${j}`;
+    gridColumn.id = `column-${j+1}`;
     gridColumn.style.flexDirection = "column";
     
-    for (let i = 0; i < gridCellNum; i++) {
+    for (let i = 0; i < gridCellNum; i++) { // creating n number of rows per column
         const gridCell = document.createElement('div');
         gridCell.style.flex = '1 1 auto'; // height and width will automatically adjust
-        gridCell.id = `row-${i}`;
+        //gridCell.style.border = "1px solid white";
+        gridCell.id = `row-${i+1}`;
         gridCell.classList.add("cell");
         gridColumn.appendChild(gridCell);
     }// for
@@ -21,32 +26,52 @@ for (let j = 0; j < gridCellNum; j++) {
     grid.appendChild(gridColumn);
 }// for
 
-let cellHover = document.querySelectorAll(".cell");
+// variables that select divs that will update/change on screen
+let cellHover = document.querySelectorAll(".cell"); // all of the cells in our grid
+let ghostModeBtn = document.querySelector("#ghost-mode"); // button for ghost mode
+let normalModeBtn = document.querySelector('#normal-mode'); // button for normal mode
+let canvas = document.querySelector("#container-1"); // container for the canvas and grid
+let screen = document.querySelector("#screen"); // screen that will be used when mouse is released
 
-// functions for drawing/undrawing the cell
+//--------------- EVENTS FOR THE CANVAS ---------------------------------------------------------
 
-function cellAppear() {
-    e.target.style.backgroundColor = "black";
-}
+// event listener for ghost mode button
+ghostModeBtn.addEventListener('click', function ghostMode() {
+    canvas.addEventListener('mousedown', () => {
+        screen.style.display = "none";
+        cellHover.forEach((itemCell) => {
+            itemCell.addEventListener('mouseover', function(e) {
+                e.target.style.backgroundColor = "black";
+                e.target.style.transition = "background-color 0s linear";
+            });
+        
+            itemCell.addEventListener('mouseout', function(e) {
+                e.target.style.backgroundColor = null;
+                e.target.style.transition = "background-color 1s linear";
+            });
+        });
+    });
+    
+    canvas.addEventListener('mouseup', () => {
+        screen.style.display = "block";
+    });
+});
 
-function cellDisappear() {
-    // COOL ANIMATION STUFF TO ADD LATER
-    e.target.style.backgroundColor = "white";
-    e.target.style.transition = "background-color 1s linear";
-}
-
-// is the cell visited?
-let cellVisited = false;
-
-cellHover.forEach((itemCell) => {
-    itemCell.addEventListener('mouseout', function(e) {
-        if (!cellVisited) {
-                    // COOL ANIMATION STUFF TO ADD LATER
-            e.target.style.backgroundColor = "black";
-            e.target.style.transition = "background-color 1s linear";
-        }
-        else {
-            
-        }
+// event listener for normal mode button
+normalModeBtn.addEventListener('click', function normalMode() {
+    canvas.addEventListener('mousedown', () => {
+        screen.style.display = "none";
+        cellHover.forEach((itemCell) => {
+            itemCell.addEventListener('mouseover', function(e) {
+                e.target.style.backgroundColor = "black";
+            });
+            itemCell.addEventListener('mouseout', function(e) {
+                e.target.style.backgroundColor = "black";
+            });
+        });
+    });
+    
+    canvas.addEventListener('mouseup', () => {
+        screen.style.display = "block";
     });
 });
